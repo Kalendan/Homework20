fc27c48b6e53cea2678147824cbfd1f595647f2e
nadja <nadja.kalenda31@gmail.com>
2024-05-19 19:54:47 +0200

    push

--- src/Massivmethods.java
+++ src/Massivmethods.java
@@ -6,6 +6,7 @@ public  class Massivmethods {
         this.array = array;
     }
 
+
     public int sum() {
         int sum = 0;
         for (int i = 0; i < 100; i++) {
@@ -20,36 +21,41 @@ public  class Massivmethods {
             if (array[i] % 2 == 0) {
                 i = array[i];
             }
-            return array[i];
         }
-        public int maxNumber() {
-            int max = array[0];
-            for (int i = 0; i < 100; i++) {
-                if (array[i] > max) {
-                    max = array[i];
-                }
-                return max;
-            }
-            public int minNumber () {
-                for (int i = 0; i < 100; i++) {
-                    int min = array[0];
-                    if (array[i] < min) {
-                        min = array[i];
-                    }
-                    return min;
-                }
+        return array[i];
+    }
 
-                public static int count () {
-                    int number = 0;
-                    for (int i = 1; i < 100; i++) {
-                        if (array[i] > array[i - 1]) {
-                            number++;
-                        }
-                        return number;
-                    }
-                }
+    public int maxNumber() {
+        int max = array[0];
+        for (int i = 0; i < 100; i++) {
+            if (array[i] > max) {
+                max = array[i];
             }
-        }}}
+        }
+        return max;
+    }
+
+    public int minNumber() {
+        int min = array[0];
+        for (int i = 0; i < 100; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+
+    }
+
+    public int count() {
+        int result = 0;
+        for (int i = 1; i < 99; i++) {
+            if (array[i] > array[i - 1] && array[i] < array[i + 1]) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
 
 
 
